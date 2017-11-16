1ccd2769be97917f155b37b6261bd717b5ab0684
Pat hayes <pat2382@yahoo.com>
2017-11-16 20:56:43 +0000

    firebase initial config set up in javascript.js

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDcxYoc_H28Y7yQAGNS1NEueEV_QAcPXW8",
+    authDomain: "traintimes-417e7.firebaseapp.com",
+    databaseURL: "https://traintimes-417e7.firebaseio.com",
+    projectId: "traintimes-417e7",
+    storageBucket: "",
+    messagingSenderId: "704515675627"
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
