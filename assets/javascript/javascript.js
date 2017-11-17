e8dff7eb782f495ad4b73d4c112b66eecc01d6e1
Joseph Aiken <josephaiken92@gmail.com>
2017-11-17 15:42:32 +0000

    add API key

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,3 +10,8 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 //test branch
+
+
+//Spoonacular API key I got from mashape
+
+var spoonKey = 'X-Mashape-Key: xsChWYIjxDmshHomTXHaaWmn7DuTp1ernr7jsnEXl2Nrg8DGIE'
