40c3097d2d0b6334372a4d817521c5c75785b345
Joseph Aiken <josephaiken92@gmail.com>
2017-11-17 15:59:49 +0000

    ajax call test

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,12 +10,19 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 //test branch
-<<<<<<< HEAD
+
 
 
 //Spoonacular API key I got from mashape
 
 var spoonKey = 'X-Mashape-Key: xsChWYIjxDmshHomTXHaaWmn7DuTp1ernr7jsnEXl2Nrg8DGIE'
-=======
-//test second branch
->>>>>>> ae001c5f351c4a5d2c610f3ef0b1c3708e6a9b2f
+
+var queryURL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=1' + spoonKey;
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+}).done(function(response) {
+
+    console.log(response);
+})
