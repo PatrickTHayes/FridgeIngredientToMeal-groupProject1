11b78ea32f7a1d7cab6dc7f91750440eaa986953
Pat hayes <pat2382@yahoo.com>
2017-11-17 15:46:02 +0000

    changed firebase api key

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,10 @@
 var config = {
-    apiKey: "AIzaSyDcxYoc_H28Y7yQAGNS1NEueEV_QAcPXW8",
-    authDomain: "traintimes-417e7.firebaseapp.com",
-    databaseURL: "https://traintimes-417e7.firebaseio.com",
-    projectId: "traintimes-417e7",
-    storageBucket: "",
-    messagingSenderId: "704515675627"
+    apiKey: "AIzaSyB3lbSA5Y4e4StvaYtm5sno0pDad-90NeM",
+    authDomain: "groupproject1-fridgetomeal.firebaseapp.com",
+    databaseURL: "https://groupproject1-fridgetomeal.firebaseio.com",
+    projectId: "groupproject1-fridgetomeal",
+    storageBucket: "groupproject1-fridgetomeal.appspot.com",
+    messagingSenderId: "32759158700"
 };
 
 firebase.initializeApp(config);
