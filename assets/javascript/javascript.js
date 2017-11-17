233c40c5635642e3871ec80b08afd5a0db9de867
Pat hayes <pat2382@yahoo.com>
2017-11-17 16:23:32 +0000

    basic Oauth added and youtube api console log from response, status working

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,3 +10,29 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 //test branch
+$(function() {
+    //Replace indicator when we know which click event should trigger our function
+    $(".btn-large").on("click", function(e) {
+        e.preventDefault();
+        //prepare request
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#ingredients").val()), //.replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",
+            publishedAfter: "2000-01-01T00:00:00Z"
+        })
+        //execute request
+        request.execute(function(response) {
+            console.log(response);
+        })
+    })
+})
+
+function init() {
+    gapi.client.setApiKey("AIzaSyCrDLUDgfk0UO5izg05bh7tU1dIjbBmBA8");
+    gapi.client.load("youtube", "v3", function() {
+        //yt api is ready
+    })
+}
