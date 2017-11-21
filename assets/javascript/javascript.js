e9068246b803af36a2ab977402e7521e3015cb7a
Pat hayes <pat2382@yahoo.com>
2017-11-21 18:45:00 +0000

    removed more console logs, removed more empty lines, added comments on what certain code was doing

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -73,7 +73,7 @@ var config = {
     storageBucket: "groupproject1-fridgetomeal.appspot.com",
     messagingSenderId: "32759158700"
 };
-
+//initialize Firebase
 firebase.initializeApp(config);
 var database = firebase.database();
 $(function() {
@@ -108,28 +108,26 @@ $(function() {
                     //$(".carousel2").append(htmlVideo);
                 })
             }, 50)
-
-
             //initialize carousel and give parameters
             $(document).ready(function() {
-                setTimeout(function() { carouselInit() }, 3000) //wait 3 seconds before running carouselinit
+                setTimeout(function() { carouselInit() }, 1750) //wait before running carouselinit, allows videos to load and brings carousel up smoothly
             });
         })
     })
 })
 
+//Initialize carousel
 function carouselInit() {
     $('.carousel').carousel({
         //height: 500,
         padding: 100,
         shift: 50,
         dist: -100,
-
         //indicators: true, //uncomment if you want indicators, although you will have to stylize them to show.
     });
 }
 
-
+//Initialize youtube API
 function init() {
     gapi.client.setApiKey("AIzaSyCrDLUDgfk0UO5izg05bh7tU1dIjbBmBA8");
     gapi.client.load("youtube", "v3", function() {
@@ -137,18 +135,17 @@ function init() {
     })
 }
 
+//ingredients to Recipe function.. on click.. spoonacular apis are called
 $("#submitForRecipes").on('click', function(event) {
-
-    event.preventDefault();
-
-    var userIngredients = "";
+    event.preventDefault(); //dont reload page
+    var userIngredients = ""; //create string var
     if (listOfIngredients.length === 0) { //if our ingredient list is empty, ask user to put some in
         $("#ingredientsLabel").html("<span style='color:red'>'Recipes require Ingredients!!!</span>");
         setTimeout(function() {
-            $("#ingredientsLabel").html("Ingredients"); //return to normal
+            $("#ingredientsLabel").html("Ingredients"); //return to normal after 2.5 seconds
         }, 2500)
     }
-
+    //build string variable by going through array of ingredients
     for (var i = 0; i < listOfIngredients.length; i++) {
         userIngredients += listOfIngredients[i];
         if (i != listOfIngredients.length - 1) {
@@ -157,8 +154,6 @@ $("#submitForRecipes").on('click', function(event) {
     }
 
 
-
-    console.log(userIngredients);
     //URL set up using jquery param method and plugging in users ingredients into URL parameters
     var queryURL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients';
     queryURL += '?' + $.param({
@@ -172,38 +167,37 @@ $("#submitForRecipes").on('click', function(event) {
 
 
     }); //fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=1'
-
+    //Ajax call after our query has been set up
     $.ajax({
         url: queryURL,
         headers: { 'X-Mashape-Key': 'xsChWYIjxDmshHomTXHaaWmn7DuTp1ernr7jsnEXl2Nrg8DGIE' },
         method: 'GET'
     }).done(function(response) {
-        $("#recipesGoHere").html(""); //clear out recipes div when called
+        $("#recipesGoHere").html(""); //clear out recipes div when called in case of old recipes
         var results = response;
         // Looping over every result item
         for (var i = 0; i < results.length; i++) {
 
 
-            var recipeDiv = $("<div class='item'>");
-            var id = results[i].id
-            getRecipe(id, i, recipeDiv);
-            //store the results here
-            var title = results[i].title;
+            var recipeDiv = $("<div class='item'>"); //create a var with div element and a class
+            var id = results[i].id //get id and store as a variable
+            getRecipe(id, i, recipeDiv); //call the getRecipe function, pass the id, div, and what number of the array were on.
+            var title = results[i].title; //store the results here
             title = "how to make " + title; //add how to make to recipe title to bring up more relevant results
-            var image = results[i].image;
+            var image = results[i].image; //grab image from ajax call
+
             //Validation logic of title
             var findHashtag = title.search("#"); //find if title has a hashtag.
             //Hashtags usually have multiple words strung together without spaces, bringing search results to 0.
             //if a title does have a hashtag, we dont want to display that title.
             if (findHashtag !== -1) {
-                // if hash tag exists we dont want  to execute rest of code, skip this iteration
-                continue;
+                continue; // if hash tag exists we dont want  to execute rest of code, skip this iteration of loop
             }
             var uriTitle = title.replace(/\(.+?\)/g, ''); //replace parentheses with +
             uriTitle = uriTitle.replace(/[^a-z0-9+]+/gi, ' '); //replace all non  a-z 0-9 with a space
             uriTitle = encodeURIComponent(uriTitle).replace(/%20/g, '+'); //encode to uri and change encoded spaces to +
 
-            // Creating a paragraph tag with recipe title
+            // Creating a paragraph tag with recipe title and attr
             var p = $("<p class='individualRecipes'>").text("title: " + title);
             p.attr("data-title", uriTitle);
 
@@ -216,43 +210,34 @@ $("#submitForRecipes").on('click', function(event) {
             // prepend the recipeDiv to the "#recipesGoHere" div in the HTML
             $("#recipesGoHere").prepend(recipeDiv);
 
-
             // when you hover over the image it changes opacity
             $('img').hover(function() {
 
-                    $(this).css('opacity', 0.5);
-                    $(this).text("test");
-                }, function() {
-                    $(this).css('opacity', 1);
-                }
-
-
-            );
-
-
+                $(this).css('opacity', 0.5);
+                $(this).text("test");
+            }, function() {
+                $(this).css('opacity', 1);
+            });
         }
     })
-
+    //get recipe function, different ajax spoonacular call
     function getRecipe(id, recipeNumber, recipeDiv) {
+        //place recipe id in query url
         var queryURL2 = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' + id + '/information';
+        //set rest of params
         queryURL2 += '?' + $.param({
-
-
             'includeNutrition': false
-
         });
+        //ajax call
         $.ajax({
             url: queryURL2,
             headers: { 'X-Mashape-Key': 'xsChWYIjxDmshHomTXHaaWmn7DuTp1ernr7jsnEXl2Nrg8DGIE' },
             method: 'GET'
 
         }).done(function(response2) {
-            console.log(response2);
-
-
+            //store results
             var results = response2;
-
-
+            //create ul element with a class and dynamic id
             var ul = $("<ul class='dropdown-content' id='dropdown" + recipeNumber + "'>") //.text("ingredients: ");
             // <!-- Dropdown Trigger -->
             var dropdownList = $("<a>"); // class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
@@ -262,15 +247,13 @@ $("#submitForRecipes").on('click', function(event) {
             dropdownList.text("ingredients");
             //ul.addId("dropdown"+recipeNumber);
             var ingredientNames = results.extendedIngredients
+            //loop through and create list items of ingrendients for this recipe
             for (var i = 0; i < ingredientNames.length; i++) {
-
                 var li = "<li>" + "-" + ingredientNames[i].name + "</li>";
                 ul.append(li);
             }
-
-
+            // append our ul var
             dropdownList.append(ul);
-
 
             // prepend the recipeDiv to the "#recipesGoHere" div in the HTML
             $(recipeDiv).append(dropdownList);
@@ -284,12 +267,6 @@ $("#submitForRecipes").on('click', function(event) {
                 alignment: 'left', // Displays dropdown with edge aligned to the left of button
                 stopPropagation: false // Stops event propagation
             });
-
-
         });
-
-
     }
-
-
 });
