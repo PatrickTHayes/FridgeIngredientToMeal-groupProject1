e0ca03a747e35dc8c0c76107381902054979e9e8
Pat hayes <pat2382@yahoo.com>
2017-11-21 01:40:15 +0000

    new branch, deals with clearing out old recipe list, old video list. when new list is called effectively replaces whichever was called

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -64,13 +64,15 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 //test branch
-$(function() {
+$(function() { //Function populates our videosGoHere division when called by click event.
     //Replace indicator when we know which click event should trigger our function
     $(document.body).on("click", ".individualRecipes", function(e) {
         e.preventDefault();
-        $(".carousel").html(""); //clear out old carousel videos if present
-        console.log("On click recipe has fired")
-        var queryTitle = $(this).attr("data-title");
+        $("#videosGoHere").html(""); //clear out old carousel videos if present
+        var carousel = $("<div class='carousel'>"); //create brand new carousel div element
+        $("#videosGoHere").append(carousel); // place in videosGoHere div
+        console.log("On click recipe has fired");
+        var queryTitle = $(this).attr("data-title"); //hook title of recipe
         //prepare request
         var request = gapi.client.youtube.search.list({
             part: "snippet",
@@ -84,28 +86,39 @@ $(function() {
         //execute request
         request.execute(function(response) {
             //console.log(response);
-            var results = response.result;
-            $.each(results.items, function(index, item) {
-                console.log(item)
-                //$("#videosGoHere").append(item.id.videoId + " " + item.snippet.title + "<br>")
-                var videoId = item.id.videoId;
-                var htmlVideo = "<a class='carousel-item' href='#one!'><div class='video-container'><iframe src='https://www.youtube.com/embed/" + videoId + "' width='560' height='315' frameborder='0' allowfullscreen></iframe></div></a>";
-                $(".carousel").append(htmlVideo);
-            })
+            setTimeout(function() { //wait before execute request so jQuery finds newly created carousel element
+                var results = response.result;
+                $.each(results.items, function(index, item) {
+                    console.log(item)
+                    //$("#videosGoHere").append(item.id.videoId + " " + item.snippet.title + "<br>")
+                    var videoId = item.id.videoId;
+                    var htmlVideo = "<a class='carousel-item' href='#one!'><div class='video-container'><iframe src='https://www.youtube.com/embed/" + videoId + "' width='560' height='315' frameborder='0' allowfullscreen></iframe></div></a>";
+                    $(".carousel").append(htmlVideo);
+                    console.log(htmlVideo)
+                    //$(".carousel2").append(htmlVideo);
+                })
+            }, 50)
+
+
             //initialize carousel and give parameters
             $(document).ready(function() {
-                $('.carousel').carousel({
-                    //height: 500,
-                    padding: 100,
-                    shift: 50,
-                    dist: -100,
-
-                    indicators: true,
-                });
+                setTimeout(function() { carouselInit() }, 3000) //wait 3 seconds before running carouselinit
             });
         })
     })
 })
+
+function carouselInit() {
+    $('.carousel').carousel({
+        //height: 500,
+        padding: 100,
+        shift: 50,
+        dist: -100,
+
+        indicators: true,
+    });
+}
+
 
 function init() {
     gapi.client.setApiKey("AIzaSyCrDLUDgfk0UO5izg05bh7tU1dIjbBmBA8");
@@ -149,6 +162,7 @@ $("#submitForRecipes").on('click', function(event) {
         headers: { 'X-Mashape-Key': 'xsChWYIjxDmshHomTXHaaWmn7DuTp1ernr7jsnEXl2Nrg8DGIE' },
         method: 'GET'
     }).done(function(response) {
+        $("#recipesGoHere").html(""); //clear out recipes div when called
         //console.log(response);
         var results = response;
         // Looping over every result item
@@ -161,6 +175,7 @@ $("#submitForRecipes").on('click', function(event) {
             getRecipe(id, i, recipeDiv);
             //store the results here
             var title = results[i].title;
+            title = "how to make " + title; //add how to make to recipe title to bring up more relevant results
             var image = results[i].image;
             var uriTitle = encodeURIComponent(title).replace(/%20/g, '+');
             console.log(uriTitle);
